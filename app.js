484432b6e2944586889d8c4556ba63bb650c4f23
Keshav Agrawal <Keshavagrawal9471408042@gmail.com>
2023-08-20 18:24:22 +0530

    Basic css included

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-    
+
 
 const app = express();
 app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 function formatTimezoneShift(timezoneShiftInSeconds) {
   const currentTimestamp = Date.now(); // Current timestamp in local time
